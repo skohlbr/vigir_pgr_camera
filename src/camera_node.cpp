e9de6d5aa48297cb4fcb6c81b60108abda07c7f0
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2014-06-28 06:24:44 +0200

    -Limit default number of worker threads to 2 for node version

--- src/camera_node.cpp
+++ src/camera_node.cpp
@@ -7,10 +7,15 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "pgr_camera");
 
+  ros::NodeHandle nh("~");
+  nh.setParam("num_worker_threads", 2);
+
   // Shared parameters to be propagated to nodelet private namespaces
   nodelet::Loader manager(true); // Don't bring up the manager ROS API
   nodelet::M_string remappings;
   nodelet::V_string my_argv;
+
+
 
   manager.load(ros::this_node::getName(), "pgr_camera/camera_nodelet", remappings, my_argv);
 
